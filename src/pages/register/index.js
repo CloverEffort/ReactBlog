f32c7d64b625d06a194ffb0ba5baec6fbcfdbf59
clover Liu <clover@notionbase.com>
2020-10-26 21:41:17 +0800

    style: register

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,8 +1,12 @@
 import { Register } from '@components';
 // import styles from './index.css';
+import styles from './index.css';
 
 export default function () {
   return (
+    <div className={styles.content}>
     <Register />
+
+    </div>
   );
 }
